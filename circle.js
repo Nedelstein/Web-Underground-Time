3888190257243f5c4a33c6f9c072a1c24df63d39
Noah Edelstein <Noedelstein@gmail.com>
2019-11-05 22:17:20 -0500

    added p5 webgl. shader canvas needs to be fixed

--- circle.js
+++ circle.js
@@ -1,40 +1,61 @@
-// function init() {
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+// // function init() {
+// var scene = new THREE.Scene();
+// var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+// var renderer = new THREE.WebGLRenderer();
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: 0x0000ff, wireframe: false });
-var cube = new THREE.Mesh(geometry, material);
-
-
-camera.position.z = 10;
-// scene.add(cube);
+// var geometry = new THREE.BoxGeometry(1, 1, 1);
+// var material = new THREE.MeshBasicMaterial({ color: 0x0000ff, wireframe: false });
+// var cube = new THREE.Mesh(geometry, material);
 
 
+// camera.position.z = 10;
+// // scene.add(cube);
 
-var geometry = new THREE.CircleGeometry(2, 12);
-var material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-var circle = new THREE.Mesh(geometry, material);
-scene.add(circle);
+
+
+// var geometry = new THREE.CircleGeometry(2, 12);
+// var material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+// var circle = new THREE.Mesh(geometry, material);
+// scene.add(circle);
+// // }
+
+// var update = function () { }
+
+// var render = function () {
+//     renderer.render(scene, camera);
 // }
 
-var update = function () { }
+// var GameLoop = function () {
+//     requestAnimationFrame(GameLoop);
 
-var render = function () {
-    renderer.render(scene, camera);
+//     update();
+//     render();
+// }
+
+// GameLoop();
+
+// // init();
+
+
+// const existingCanvas = document.getElementById('canvas');
+
+function setup() {
+    const cnv = createCanvas(windowWidth, windowHeight, WEBGL);
+    cnv.id("myCanvas");
+
+
 }
 
-var GameLoop = function () {
-    requestAnimationFrame(GameLoop);
-
-    update();
-    render();
+function draw() {
+    background(127);
+    push();
+    translate(-width / 2, -height / 2, 0)
+    noStroke();
+    fill(255);
+    ellipse(windowWidth / 2, windowHeight / 2, 100, 100);
+    pop();
 }
 
-GameLoop();
-
-// init();
