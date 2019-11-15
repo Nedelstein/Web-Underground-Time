2111f1a14d7be7d1bdad8af907353c6fd7dd46a2
Noah Edelstein <Noedelstein@gmail.com>
2019-11-15 18:29:28 -0500

    transparent canvas, no easycam

--- p5Circle.js
+++ p5Circle.js
@@ -7,20 +7,20 @@ var currentTime;
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight, WEBGL);
-  createEasyCam();
+  // createEasyCam();
 
-  c1 = color(255, 255, 255);
-  c2 = color(24, 180, 245);
-  c3 = color(4, 172, 114); //3time
-  c4 = color(254, 242, 169);
-  c5 = color(1, 168, 239);
-  c6 = color(247, 206, 92);
-  c7 = color(21, 143, 200);
-  c8 = color(79, 67, 82);
-  c9 = color(211, 135, 35);
-  c10 = color(102, 136, 176);
-  c11 = color(190, 116, 105); //twice
-  c12 = color(79, 67, 82);
+  c1 = color(255, 255, 255, 180);
+  c2 = color(24, 180, 245, 180);
+  c3 = color(4, 172, 114, 180); //3time
+  c4 = color(254, 242, 169, 180);
+  c5 = color(1, 168, 239, 180);
+  c6 = color(247, 206, 92, 180);
+  c7 = color(21, 143, 200, 180);
+  c8 = color(79, 67, 82, 180);
+  c9 = color(211, 135, 35, 180);
+  c10 = color(102, 136, 176, 180);
+  c11 = color(190, 116, 105, 180); //twice
+  c12 = color(79, 67, 82, 180);
   let currentTime = hour();
   console.log("Current Hour:" + currentTime);
 }
@@ -83,10 +83,11 @@ numDots = total * total;
 
 function draw() {
   background(0, 0);
-  // push();
-  // translate(-width / 2, -height / 2, -100);
-  // TimetoGrad();
-  // pop();
+  smooth();
+  push();
+  translate(-width / 2, -height / 2);
+  TimetoGrad();
+  pop();
 
   for (let i = 0; i < numDots; i++) {
     noFill();
@@ -163,7 +164,7 @@ function TimetoGrad() {
     setGradient(c10, c11);
   }
   if (currentTime == 18) {
-    setGradient(c11, c12);
+    setGradient(c5, c6);
   }
   if (currentTime == 19) {
     setGradient(c12, c11);
