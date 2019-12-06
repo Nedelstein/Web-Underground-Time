52b59800880e407ea9de28e8d4bc8a976f53bc1f
Noah Edelstein <Noedelstein@gmail.com>
2019-12-05 21:14:33 -0500

    circle working. need to fix others

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -27,6 +27,7 @@ module.exports = {
   output: {
     // filename: "[name].bundle.js",
     filename: `${versionUUID}/[hash].js`,
+    filename: `${versionUUID}/[hash].js`,
     path: path.resolve(__dirname, "../docs")
   },
   module: {
